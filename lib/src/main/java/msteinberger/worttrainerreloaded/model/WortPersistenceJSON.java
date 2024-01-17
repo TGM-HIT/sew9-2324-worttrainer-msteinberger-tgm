56ce8342f50d574494c932cb1836988d2bf0be7a
MasterMarcoHD <57987974+MasterMarcoHD@users.noreply.github.com>
2024-01-17 10:39:28 +0100

    implemented persistence
    Create README.md

---
+++ lib/src/main/java/msteinberger/worttrainerreloaded/model/WortPersistenceJSON.java
@@ -0,0 +1,58 @@
+package msteinberger.worttrainerreloaded.model;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.filechooser.FileSystemView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+public class WortPersistenceJSON extends WortPersistence {
+	private final String path = "/Documents/WortTrainer/worttrainer1.txt";
+	
+	public void save(WortChecker wChecker) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		File f = new File(WortPersistence.getSystemPath() + path);
+		if(!f.isFile()) {
+			try {
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		try(BufferedWriter out = new BufferedWriter(new FileWriter(f))) {
+			out.write(gson.toJson(wChecker));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public WortChecker load() {
+		WortChecker wChecker = null;
+		File f = new File(WortPersistence.getSystemPath() + path);
+		if(f.isFile()) {
+			try(BufferedReader in = new BufferedReader(new FileReader(f))) {
+				String json = "";
+				String text;
+				while((text = in.readLine()) != null) {
+					json += text;
+				}
+				wChecker = new Gson().fromJson(json, WortChecker.class);
+			} catch (IOException e) {
+				wChecker = null;
+				e.printStackTrace();
+			}
+		}
+		return wChecker;
+	}
+}
